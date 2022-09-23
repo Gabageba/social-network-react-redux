43d0426ade8150e0ea2b972cbddcc5b686742b40
Gabageba <gabageba@gmail.com>
2022-09-23 20:27:04 +0700

    callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import {state} from './redux/state'
+import {addPost, sendMessage, state, subscribe, updateNewMessageText, updateNewPostText} from './redux/state'
 import './index.css'
-import {rerenderEntireTree} from './render'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import App from './App'
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App state={state}
+           addPost={addPost}
+           sendMessage={sendMessage}
+           updateNewMessageText={updateNewMessageText}
+           updateNewPostText={updateNewPostText}
+      />
+    </React.StrictMode>
+  )
+}
 
 rerenderEntireTree(state)
+subscribe(rerenderEntireTree)
 
