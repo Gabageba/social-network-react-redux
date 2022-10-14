725e2c3ad0bdbb1d0cf6ae84fe4e593d863a2ab5
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2022-10-14 18:48:48 +0700

    users API

--- src/pages/UsersPage/UserCard/UserCard.jsx
+++ src/pages/UsersPage/UserCard/UserCard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import AddFriendSvg from '../../../components/svgFiles/AddFriendSvg'
 import styles from './UserCard.module.css'
 import DeleteFriendSvg from '../../../components/svgFiles/deleteFriendSvg'
+import noImage from '../../../assets/noImageLarge.png'
 
 const UserCard = ({user, unfollow, follow}) => {
   return (
     <div className={styles.userCard}>
       <img width={160} height={160} style={{borderRadius: '3px'}}
-           src={user.img} alt="Avatar"/>
+           src={user.photos.large !== null ? user.photos.large : noImage} alt="Avatar"/>
       <div className={styles.infoBlock}>
         <p className={styles.name}>{user.name}</p>
         {
