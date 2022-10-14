61863abd73169164f5d2bdac9e5c95edcef07409
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2022-10-14 19:00:00 +0700

    Side effect

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -5,13 +5,13 @@ import UserCard from './UserCard/UserCard'
 import axios from 'axios'
 
 const UsersPage = ({usersData, follow, unfollow, setUsers}) => {
+  const getUsers = () => {
+    if (usersData.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(users => setUsers(users.data.items))
 
-  if (usersData.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(users => setUsers(users.data.items))
-
+    }
   }
-
   return (
     <div className={styles.findFriendPage}>
       <div className={`contentBlock ${styles.users}`}>
@@ -19,6 +19,7 @@ const UsersPage = ({usersData, follow, unfollow, setUsers}) => {
           <h4>Поиск друзей</h4>
         </div>
         <input type="text" placeholder={'Введите запрос'} className={styles.input}/>
+        <button onClick={getUsers}>Get users</button>
         <div className={styles.cards}>
           {
             usersData.map(user => {
