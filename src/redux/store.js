dec2716945c65ead4658ad1aa519259aafd92d0f
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2022-11-19 22:23:35 +0700

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
-import {combineReducers, createStore} from 'redux'
-import {profileReducer} from './profileReducer'
-import {dialogsReducer} from './dialogsReducer'
-import {usersReducer} from './usersReducer'
+import { combineReducers, createStore } from 'redux'
+import { profileReducer } from './profileReducer'
+import { dialogsReducer } from './dialogsReducer'
+import { usersReducer } from './usersReducer'
 
 const reducers = combineReducers({
+  //объеденить reducer
   profile: profileReducer,
   dialogs: dialogsReducer,
-  users: usersReducer
+  users: usersReducer,
 })
 
 export const store = createStore(reducers)
