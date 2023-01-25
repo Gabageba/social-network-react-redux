b63855c4f9b2b6570a530af31acde1641fb468e4
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-01-25 18:13:36 +0700

    textarea autosize, NewPostTextArea

--- src/pages/ProfilePage/NewPost/NewPost.jsx
+++ src/pages/ProfilePage/NewPost/NewPost.jsx
@@ -9,23 +9,23 @@ const NewPost = ({addPost, postWriting, setPostWriting}) => {
     setPostWriting(false)
   }
   return (
-    <div className={`${styles.newPost} contentBlock`}>
+    <>
       {
         postWriting
           ?
           <ClickAwayListener onClickAway={() => setPostWriting(false)} >
-            <div>
+            <div style={{marginBottom: '20px'}}>
               <NewPostReduxForm onSubmit={onSubmit}/>
             </div>
           </ClickAwayListener>
           :
-          <div style={{display: 'flex'}} onClick={() => setPostWriting(true)}>
+          <div className={`${styles.newPost} contentBlock`} onClick={() => setPostWriting(true)}>
             <img className={styles.avatar} src="https://i.pinimg.com/736x/86/95/54/8695540db1e9224367ed9d1a4884ccfc.jpg"
                  alt="Avatar"/>
             <p className={styles.newPostText}>Что нового чел?</p>
           </div>
       }
-    </div>
+    </>
   )
 }
 
--- src/pages/ProfilePage/NewPost/NewPostReduxForm.jsx
+++ src/pages/ProfilePage/NewPost/NewPostReduxForm.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import styles from './NewPost.module.css'
 import {Field, reduxForm} from 'redux-form'
+import {maxLength, required} from '../../../utils/validators'
+import {NewPostTextarea} from '../../FormsControls/FormsControls'
+
+const maxLength100 = maxLength(100)
 
 const NewPostForm = ({handleSubmit}) => {
   return (
     <form onSubmit={handleSubmit}>
-      <div style={{display: 'flex'}}>
-        <img className={styles.avatar}
-             src="https://i.pinimg.com/736x/86/95/54/8695540db1e9224367ed9d1a4884ccfc.jpg"
-             alt="Avatar"/>
-        <Field placeholder={'Что нового чел?'}
-               component={'textarea'}
+      <div>
+        <Field component={NewPostTextarea}
+               placeholder={'Что нового чел?'}
                name={'newPostText'}
                autoFocus={true}
-               className={`${styles.input} ${styles.newPostText}`}/>
+               validate={[required, maxLength100]}
+               className={`${styles.input} ${styles.newPostText}`} />
       </div>
-      <div style={{textAlign: 'right'}}>
+      <div className={`contentBlock ${styles.postButtonBlock}`}>
         <button className={styles.postButton}>Опубликовать</button>
       </div>
     </form>
